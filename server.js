86b1827fc3689275a1056b44ada160aa4571e72b
Mike Kotte <kotte.mike@gmail.com>
2019-04-13 11:22:04 +0200

    cleanup

--- server.js
+++ server.js
@@ -6,7 +6,20 @@ import cors from 'cors';
 
 import routes from './exports/routes/'
 
-const app = express().use(urlencoded({ extended: true })).use(json()).use(cors({ origin: '*', optionsSuccessStatus: 204 })).use(Staticworkaround('public')).use(helmet()).options('*', cors());
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 204
+}
+
+const app = express();
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
+app.use(cors(corsOptions));
+app.use(Staticworkaround('public'));
+app.use(helmet());
+
+app.options('*', cors());
 
 app.use(function (req, res, next) {
   console.log(req.body);
