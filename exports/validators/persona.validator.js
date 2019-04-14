26257062789c9d2b33a85d9411d8d1791d68cf5d
Mike Kotte <kotte.mike@gmail.com>
2019-04-14 08:54:33 +0200

    extra else

--- exports/validators/persona.validator.js
+++ exports/validators/persona.validator.js
@@ -12,16 +12,17 @@ const personaData = Joi.object().keys({
 });
 
 export const cookieIdValidator = (req, res, next) => {
-  if (!req.signedCookies.persona_tag)
-    res.status(400).send({ success: false, msg: 'No cookie present' });
-  
-  personaId.validate(req.signedCookies.persona_tag, { abortEarly: false })
-    .then(() => {
-      next();
-    })
-    .catch(validationError => {
-      res.status(200).send({ success: false, msg: `Invalid request: ${validationError.details.map(d => d.message)}` });
-    });
+  if (!req.signedCookies.persona_tag) {
+    res.status(200).send({ success: false, msg: 'No cookie present' });
+  } else {
+    personaId.validate(req.signedCookies.persona_tag, { abortEarly: false })
+      .then(() => {
+        next();
+      })
+      .catch(validationError => {
+        res.status(200).send({ success: false, msg: `Invalid request: ${validationError.details.map(d => d.message)}` });
+      });
+  }
 }
 
 export const personaDataValidator = (req, res, next) => {
