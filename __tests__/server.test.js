8cb47b82cfd46aea58ce06ff72b2bb118b82cd3c
Mike Kotte <kotte.mike@gmail.com>
2019-04-14 09:31:00 +0200

    update

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,14 +1,14 @@
 import 'babel-polyfill';
-import request from 'supertest';
-import app from '../server';
+import { createPersona } from '../exports/services/persona.service';
+import { getOrCreatePersonaCategory } from '../exports/services/persona_category.service';
 
-describe('GET /nonexistingendpoint', () => {
-  test('It should say the endpoint does not exists', async () => {
-    return request(app)
-      .get('/nonexistingendpoint')
-      .expect(404)
-      .then(response => {
-        expect(response.text).toBe('Not a valid endpoint');
+describe('Basic CRUD', () => {
+  test('It should create a persona', async () => {
+    const category = await getOrCreatePersonaCategory("stronglang_nonvisualcom");
+    createPersona({ personaCategoryId: category[0].personaCategoryId })
+      .then(personaObject => {
+        persona = personaObject;
+        expect(personaObject.personaId).toHaveLength(36);
       });
   });
-});
+})
